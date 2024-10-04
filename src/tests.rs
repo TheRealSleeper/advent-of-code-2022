5f1a4e458fc3799e285ddd5b2394b24dcf9f1530
TheRealSleeper <140853156+TheRealSleeper@users.noreply.github.com>
2024-10-04 15:48:26 -0500

    added tests and empty input files

---
+++ src/tests.rs
@@ -0,0 +1,17 @@
+#[allow(dead_code, unused_variables)]
+#[cfg(test)]
+fn part1() {
+    let sample = std::fs::read_to_string("sample1.txt").unwrap();
+    let expected: isize = todo!();
+    let actual = super::part1(&sample);
+    assert_eq!(actual, expected);
+}
+
+#[allow(dead_code, unused_variables)]
+#[cfg(test)]
+fn part2() {
+    let sample = std::fs::read_to_string("sample2.txt").unwrap();
+    let expected: isize = todo!();
+    let actual = super::part2(&sample);
+    assert_eq!(actual, expected);
+}
