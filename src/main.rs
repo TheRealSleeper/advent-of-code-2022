f0b143a7370daa35c833d623e675d831c7c2577b
TheRealSleeper <140853156+TheRealSleeper@users.noreply.github.com>
2024-10-04 15:46:00 -0500

    both parts solved

--- src/main.rs
+++ src/main.rs
@@ -30,9 +30,28 @@ fn main() {
 }
 
 fn part1(inp: &str) -> isize {
-    todo!()
+    inp.replace("\r\n", "\n")
+        .split("\n\n")
+        .map(|e| {
+            e.lines()
+                .map(|l| l.trim().parse::<u32>().unwrap())
+                .sum::<u32>() as isize
+        })
+        .max()
+        .unwrap()
 }
 
 fn part2(inp: &str) -> isize {
-    todo!()
+    let mut answer = inp.replace("\r\n", "\n")
+        .split("\n\n")
+        .map(|e| {
+            e.lines()
+                .map(|l| l.trim().parse::<u32>().unwrap())
+                .sum::<u32>() as isize
+        })
+        .collect::<Vec<isize>>(); 
+    
+    answer.sort();
+
+    answer.last_chunk::<3>().unwrap().iter().sum()
 }
