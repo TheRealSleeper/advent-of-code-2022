c4c7fc4a0a2bc354e78c1dc666f217ffcc22cac5
TheRealSleeper <asleeper135@gmail.com>
2024-10-04 20:52:27 -0500

    solved both parts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
 mod args;
@@ -27,12 +28,53 @@ fn main() {
     if options.part2 {
         println!("{}", part2(&content));
     }
+
+    part1(&content);
 }
 
 fn part1(inp: &str) -> isize {
-    todo!()
+    let priority_map: HashMap<char, usize> = ('a'..='z')
+        .chain('A'..='Z')
+        .enumerate()
+        .map(|x| (x.1, x.0 + 1))
+        .collect();
+
+    inp.lines()
+        .map(|l| {
+            let first_compartment: HashMap<char, _> =
+                l[0..l.len() / 2].chars().map(|c| (c, 0)).collect();
+
+            let second_compartment: HashMap<char, _> =
+                l[l.len() / 2..l.len()].chars().map(|c| (c, 0)).collect();
+
+            for (c, _) in first_compartment {
+                if second_compartment.contains_key(&c) {
+                    return priority_map[&c] as isize;
+                }
+            }
+
+            unreachable!("No duplicate was found!")
+        })
+        .sum::<isize>()
 }
 
 fn part2(inp: &str) -> isize {
-    todo!()
+    let priority_map: HashMap<char, usize> = ('a'..='z')
+        .chain('A'..='Z')
+        .enumerate()
+        .map(|x| (x.1, x.0 + 1))
+        .collect();
+
+    inp.lines()
+        .collect::<Vec<&str>>()
+        .chunks_exact(3)
+        .map(|group| {
+            for c in group[0].chars() {
+                if group[1].contains(c) && group[2].contains(c) {
+                    return priority_map[&c] as isize;
+                }
+            }
+            unreachable!("No badge found!")
+        })
+        .sum::<isize>()
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,17 +1,17 @@
 #[allow(dead_code, unused_variables)]
-#[cfg(test)]
+#[test]
 fn part1() {
     let sample = std::fs::read_to_string("sample1.txt").unwrap();
-    let expected: isize = todo!();
+    let expected: isize = 157;
     let actual = super::part1(&sample);
     assert_eq!(actual, expected);
 }
 
 #[allow(dead_code, unused_variables)]
-#[cfg(test)]
+#[test]
 fn part2() {
     let sample = std::fs::read_to_string("sample2.txt").unwrap();
-    let expected: isize = todo!();
+    let expected: isize = 70;
     let actual = super::part2(&sample);
     assert_eq!(actual, expected);
 }
